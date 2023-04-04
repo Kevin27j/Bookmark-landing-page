fb78aea1ed4abe1dad809198a2699610ef91c8c4
Kejvin Pashaj <kevin.952011@gmail.com>
2023-04-04 15:41:59 +0100

    added basic styling

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,12 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+// Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon
 function myFunction() {
-    var x = document.getElementById("links-menu");
-    if (x.style.display === "block") {
-      x.style.display = "none";
+    let x = $("#links-menu");
+    if (x.css("display") === "block") {
+      x.css("display", "none");
     } else {
-      x.style.display = "block";
+      x.css("display", "block");
     }
   }
+
+  // Show tab based on list selection
+  
