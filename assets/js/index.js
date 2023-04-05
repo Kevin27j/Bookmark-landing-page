76cf660c86ceadb33e776a61b07e3d86166cadd9
Kejvin Pashaj <kevin.952011@gmail.com>
2023-04-05 13:21:27 +0100

    added functionality to features section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,28 @@
 // Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon
-function myFunction() {
-    let x = $("#links-menu");
-    if (x.css("display") === "block") {
-      x.css("display", "none");
-    } else {
-      x.css("display", "block");
-    }
-  }
+const toggleIcon = $(".toggle-icon");
 
-  // Show tab based on list selection
-  
+toggleIcon.click(function () {
+  let x = $("#links-menu");
+  if (x.css("display") === "block") {
+    x.css("display", "none");
+  } else {
+    x.css("display", "block");
+  }
+})
+
+// Show features tab based on list selection
+const featureList = $(".list-item");
+const featureTabs = $(".feature-wrapper > div");
+
+for (let i = 0; i < featureList.length; i++) {
+  featureList.eq(i).click(function () {
+    // Toggle red underline when click on a features list link
+    featureList.find("a.active-list").removeClass("active-list");
+    featureList.eq(i).find("a").toggleClass("active-list");
+    // Display and hide tabs based on list selection
+    featureTabs.removeClass("show").addClass("hide");
+    featureTabs.eq(i).removeClass("hide").addClass("show");
+  })
+}
+
+console.log()
