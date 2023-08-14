79b6731af8e13968d9cbaf87e40f7d6efe3ce880
Kejvin Pashaj <kevin.952011@gmail.com>
2023-08-14 13:59:16 +0100

    email form error background

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,27 @@
 // NAVBAR TOGGLE FUNCTION
 // Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon
 const openIcon = $(".open-icon");
-const closeIcon =$(".close-icon");
+const closeIcon = $(".close-icon");
 const linksWrapper = $(".links-wrapper");
-const linksMenu = $(".links-menu")
+const linksMenu = $(".links-menu");
 const links = $(".links-menu a");
 const socialLinks = $(".social-links");
 
 openIcon.click(function () {
   // Open overlay menu
-  if(linksWrapper.css("display") === "none") {
+  if (linksWrapper.css("display") === "none") {
     linksWrapper.css("display", "block");
-    
+
     // Close overlay menu
-    closeIcon.click(function(){
-      if(linksWrapper.css("display") === "block") {
+    closeIcon.click(function () {
+      if (linksWrapper.css("display") === "block") {
         linksWrapper.css("display", "none");
       }
     })
 
     // Close menu if click on menu link
-    links.click(function(){
-      if(linksWrapper.css("display") === "block") {
+    links.click(function () {
+      if (linksWrapper.css("display") === "block") {
         linksWrapper.css("display", "none");
       }
 
@@ -63,6 +63,7 @@ const form = $(".form");
 const emailInput = $(".email-input");
 const emailButton = $(".form-button");
 const emailError = $(".email-error");
+const errorBackground = $("#error-background");
 
 // Validate email
 const emailRgx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -71,15 +72,24 @@ emailButton.click(function () {
   // if user input is not an email show error message
   if (!emailInput.val().match(emailRgx) || emailInput.val() === "") {
     emailError.css("display", "block");
+    errorBackground.css({
+      "background-color": "var(--soft-red)",
+      "border": "2px solid var(--soft-red)",
+      "border-radius": "5px"
+    })
     // Remove error message when user clicks on email input
     emailInput.click(function () {
       emailError.css("display", "none");
+      errorBackground.css({
+        "background-color": "none",
+        "border": "none",
+      })
     })
   }
   // else if user input is a valid email
   else {
-    emailError.css("display", "none")
-    alert("Great Success")
+    emailError.css("display", "none");
+    alert("Great Success");
     form.submit(function (event) {
       event.preventDefault();
     });
